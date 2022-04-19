08900d3be0ef9cef7815e5bf53fe18dab7d4cfff
Max Fukui <max.fukui.dev@gmail.com>
2022-04-19 17:57:32 -0400

    implemeting link and basic routes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import Link from 'next/link';
+
 function HomePage(){
   return (
     <div>
       <h1> Hello World </h1>
+      <ul>
+        <li>
+          <Link href="/portfolio"></Link>
+        </li>
+        <li>
+          <Link>Clientes</Link>
+        </li>
+      </ul>
     </div>
   )
 }
